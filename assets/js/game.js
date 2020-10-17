32c3db1a3f417b4a54f8e16fb11bfe3ccbc2c9b3
nathan-potts41 <pottsandpans41@hotmail.com>
2020-10-16 23:49:22 -0500

    start and end game function

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,6 +12,8 @@ var enemyAttack = 12;
 
 window.alert("Welcome to Robot Gladiators Colosseum of Death! *thunderous applause* ");
 
+//startGame() {}
+
 var fight = function (enemyName) {
     
     var promptFight = window.prompt("Would you like to FIGHT or COWARD out of this battle? Enter 'FIGHT' or 'COWARD' to choose.");
@@ -64,9 +66,56 @@ var fight = function (enemyName) {
     }
 };
 
-for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+// endGame();
+// window.alert(playerName + " has defeated the Gladiator Arena with " + playerHealth " health, " + playerMoney " rubels, and " + playerAttack " attack power." );
+// console.log(playerName, playerHealth, playerMoney, playerAttack);
+// window.prompt("Would you like to play again? Enter Yes or No.")
+var startGame = function() {
+    // reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+
+    for(var i = 0; i < enemyNames.length; i++) {
+        if (playerHealth > 0) {
+            window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+            var pickedEnemyName = enemyNames[i];
+            
+            enemyHealth = 50;
+
+            fight(pickedEnemyName);
+        }
+        else {
+            window.alert("You have lost your robot battle! Game Over!");
+            break;
+        }
+    }
+    endGame();
+};
+
+//function to end entire game
+var endGame = function() {
+    window.alert("The game has now concluded. Let's see how you did!");
+    if (playerHealth > 0) {
+        window.alert("Great job, you've survived the arena! You now have a score of "  + playerMoney + ".");
+    }
+    else {
+        window.alert("You robot is now scrap metal.");
+    }
+    //ask player if they'd like to try again
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    if (playAgainConfirm) {
+        //restart game
+        startGame();
+    }
+    else {
+        window.alert("Thanks for playing Robot Gladiators! Come back when you've got the guts to win.");
+    }
 }
+
+startGame();
 
 // Game States
 // "WIN" - Player robot has defeated all enemy-robots
